35622e8a79e5c2f25ae9423a68d188a24e6960cd
amparoq <aquiroz@miuandes.cl>
2023-06-08 15:56:41 -0400

    create ticket listo (falta hacer un attachment y mostrarlo en el show del ticket de haber)

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -33,7 +33,30 @@ class TicketsController < ApplicationController
 
   # POST /tickets or /tickets.json
   def create
-    @ticket = Ticket.new(ticket_params)
+    title = params[:title]
+    description = params[:description]
+    requiring_user_id = current_user.id
+    priority = params[:priority]
+    creation_date = Date.today
+    deadline_date = nil
+
+    if priority == '1'   
+      deadline_date = creation_date + 3
+    elsif priority == '2'
+      deadline_date = creation_date + 5
+    elsif priority == '3'
+      deadline_date = creation_date + 7
+    end
+
+    executive_users = User.executive.joins(:executive_tickets).group('users.id').order('COUNT(tickets.id) ASC')
+    executive_id = executive_users.first.id
+    @ticket = Ticket.create(title: title, description: description, creation_date: creation_date, deadline_date: deadline_date, state: "open", priority: priority, executive_id: executive_id, requiring_user_id: requiring_user_id)
+    tags = params[:tags]
+    if tags.present?
+      tags.each do |tag|
+        TagsTicket.create(ticket_id: @ticket.id, tag_id: tag)
+      end
+    end
 
     respond_to do |format|
       if @ticket.save
@@ -77,6 +100,6 @@ class TicketsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def ticket_params
-      params.fetch(:ticket, {})
+      params.fetch(:ticket, {}).permit(:title, :description, :priority)
     end
 end
